9883603be965df988a0b86bf271f0a51831628dc
varun7537 <111139401+varun7537@users.noreply.github.com>
2023-10-03 23:32:18 +0530

    Add files via upload

---
+++ OnlineMedicalStore-main/includes/nav.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg navbar-dark" style="background: linear-gradient(to right, purple, red); height: 70px;">
+  <a class="navbar-brand m-0 m-lg-0" href="index.php"><img src="assets/MediMart.png" height="75px" width="100px" style="margin-top: 1px; margin-left: -10px; border-radius: 100px;"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="aboutus.php">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="AskOurPharmacist.php">Ask Your Pharmacist</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="upload.php">Order Prescription</a>
+      </li>
+    </ul>
+  </div>
+      <form class="form-inline my-2 my-lg-0">
+          <a href="login.php" class="btn btn-dark my-2 my-sm-0">LOGIN</a> &nbsp;
+          <a href="signup.php" class="btn btn-outline-dark my-2 my-sm-0">SIGNUP</a>
+      </form>
+</nav>
